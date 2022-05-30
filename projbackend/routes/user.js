f46efb544b2114ca14c1e0b7c27f7f6911c418e4
junaidapdev <junaidap.dev.com>
2022-05-30 22:23:16 +0530

    added userPurchaseList

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser } = require("../controllers/user");
+const { getUserById, getUser, userPurchaseList } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+
+router.get("/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
 
 module.exports = router;
