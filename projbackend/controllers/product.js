8d0ea5bfb76ae6680486f3d300a136b8c6215dc7
junaidapdev <junaidap.dev.com>
2022-06-01 23:15:52 +0530

    update inventory

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -154,3 +154,23 @@ exports.deleteProduct = (req, res) => {
         res.json(products);
       });
   };
+
+  exports.updateStock = (req, res, next) => {
+      let myOperations = req.body.order.products.map(prod => {
+          return {
+              updateOne: {
+                  filter: {_id: prod._id},
+                  update: {$inc: {stock: -prod.count, sold: +prod.count}}
+              }
+          }
+      })
+
+      Product.bulkWrite(myOperations, {}, (err, products) => {
+          if(err){
+              return res.status(400).json({
+                  error: "Bulk Operations"
+              })
+          }
+          next()
+      })
+  }
