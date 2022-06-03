b52482924adde2978f27ccf0bff6236cdf5fdb83
junaidapdev <junaidap.dev.com>
2022-06-03 15:57:09 +0530

    inject backend to frontend

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import '../styles.css'
+import '../styles.css' 
+import { API } from '../backend';
 const Home = () => {
+
+    console.log("API IS", API);
+
   return (
     <div className='text-white'>Home</div>
   )
