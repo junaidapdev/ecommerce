c1b0b8aff50c46a751fc8f19d73176021d126569
junaidapdev <junaidap.dev.com>
2022-06-05 11:18:47 +0530

    signup logic done

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -78,11 +78,44 @@ const Signup = () => {
     );
   };
 
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New account was created successfully. Please
+            <Link to="/signin">Login Here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Base title="Sign up page" description="A page for user to sign up!">
+      {successMessage()}
+      {errorMessage()}
       {signUpForm()}
-      <p className="text-light text-center">{JSON.stringify(values)}</p>
-    </Base> 
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
+    </Base>
   );
 };
 
