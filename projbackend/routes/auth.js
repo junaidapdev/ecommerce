c16c5e65c96a1b4a210c95a7f05122b2906e5949
junaidapdev <junaidap.dev.com>
2022-06-05 11:06:15 +0530

    fixed bugs

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,15 +2,18 @@ var express = require("express");
 const { check } = require("express-validator");
 var router = express.Router();
 
-const { signout, signup, signin, isSignedIn } = require('../controllers/auth')
-
-router.post("/signup",[
-    check('name', 'name must be atleast 3 characters').isLength({min: 3}),
-    check('email', 'email is required').isEmail(),
-    check('password', 'password must be 3 char').isLength({min: 3}),
-], signup)
+const { signout, signup, signin } = require('../controllers/auth')
 
 
+router.post(
+    "/signup",
+    [
+      check("name", "name should be at least 3 char").isLength({ min: 3 }),
+      check("email", "email is required").isEmail(),
+      check("password", "password should be at least 3 char").isLength({ min: 3 })
+    ],
+    signup
+  );
 router.post("/signin",[
     check('email', 'email is required').isEmail(),
     check('password', 'password field is requires').isLength({min: 1}),
@@ -21,10 +24,10 @@ router.post("/signin",[
 
 router.get("/signout", signout)
 
-router.get("/testroute", isSignedIn ,(req, res) => {
-    res.send("A Protectes Route")
-}
-)
+// router.get("/testroute", isSignedIn ,(req, res) => {
+//     res.send("A Protectes Route")
+// }
+// )
 
 
 module.exports = router;
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -4,62 +4,87 @@ import { Link } from "react-router-dom";
 import { signup } from "../auth/helper";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+        }
+      })
+      .catch(console.log("Error in signup"));
+  };
 
   const signUpForm = () => {
-      return(
-    <div className="row">
-      <div className="col-md-6 offset-sm-3 text-left">
-        <form>
-          <div className="form-group">
-            <label className="text-light">Name</label>
-            <input
-              className="form-control"
-              // onChange={handleChange("name")}ƒ
-              type="text"
-              // value={name}
-            />
-          </div>
-          <div className="form-group">
-            <label className="text-light">Name</label>
-            <input
-              className="form-control"
-              // onChange={handleChange("name")}ƒ
-              type="text"
-              // value={name}
-            />
-          </div>
-          <div className="form-group">
-            <label className="text-light">email</label>
-            <input
-              className="form-control"
-              // onChange={handleChange("name")}ƒ
-              type="email"
-              // value={name}
-            />
-          </div>
-          <div className="form-group">
-            <label className="text-light">Password</label>
-            <input
-              className="form-control"
-              // onChange={handleChange("name")}ƒ
-              type="password"
-              // value={name}
-            />
-          </div>
-          <button  className="btn btn-success btn-block">
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
               Submit
             </button>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-      ) 
+    );
   };
 
   return (
-    <Base title="Sign Up Page">
-        {signUpForm()}
-    </Base>
+    <Base title="Sign up page" description="A page for user to sign up!">
+      {signUpForm()}
+      <p className="text-light text-center">{JSON.stringify(values)}</p>
+    </Base> 
   );
 };
 
 export default Signup;
+ 
